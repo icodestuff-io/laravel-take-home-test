dd465439602abddf258cdc54d7c7536635fd1019
Solomon04 <antoinesolomon5@gmail.com>
2023-05-18 21:15:35 -0400

    add show form controller boilerplate

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,3 @@ use App\Http\Controllers\ShowFormController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', ShowFormController::class);
-Route::post('/submit', \App\Http\Controllers\SendEmailController::class);
